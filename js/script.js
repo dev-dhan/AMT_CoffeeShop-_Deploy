3efce114f4dd15da7c515468fb378b9b06c3c2db
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-02-18 23:11:49 +1100

    fixed go up button

--- js/script.js
+++ js/script.js
@@ -34,3 +34,20 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+
+  /**Go Up Button */
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+        } else{
+        $('#topBtn').fadeOut();
+        }
+    });
+    
+    $("#topBtn").click(function(){
+        $('html ,body').animate({scrollTop : 0},800);
+    });
+    });
